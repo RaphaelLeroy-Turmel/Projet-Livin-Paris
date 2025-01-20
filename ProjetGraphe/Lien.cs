2ecf98863f35aa06e203bee159a85e9abe0afabb
Raphael_Leroy-Turmel <raraphaelleroy.100@gmail.com>
2025-01-20 10:55:16 +0100

    .

---
+++ ProjetGraphe/Lien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetGraphe
+{
+    internal class Lien
+    {
+        private Noeud noeudA;
+        private Noeud noeudB;
+
+        public Lien(Noeud noeudA, Noeud noeudB)
+        {
+            this.noeudA = noeudA;
+            this.noeudB = noeudB;
+        }
+    }
+}
---
+++ ProjetGraphe/Noeud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetGraphe
+{
+    internal class Noeud
+    {
+        private int noeud_id;
+        private Noeud[,] relations;
+    }
+}
