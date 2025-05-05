7672018633f449951b2ab487bfcaeefee421d3a5
ThomasLiotier <thomas.liotier@edu.devinci.fr>
2025-05-05 15:26:35 +0200

    .

--- TestProject1/obj/Debug/net8.0-windows/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0-windows/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce6ca5eb989a55a8f901c08d43f3146b76435a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cddd2ff563c594e85484f3f8fe400eb8795e959e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
